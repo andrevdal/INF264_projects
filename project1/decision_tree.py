d9d097a0ad038b7e00b900d7655f76b838d73437
Amy <amykaletabe@gmail.com>
2026-09-17 23:48:03 +0200

    add structure to desition_tree.py

--- project1/decision_tree.py
+++ project1/decision_tree.py
@@ -1,7 +1,41 @@
-#1 Decision Trees -
+# 1 Decision Trees ---
 
-#1.1 ID3 ALgorithm
+import numpy as np
 
-#1.2 Gini Index
+#1.1 The ID3 Algorithm 
 
-#1.3 Maximum Depth
+class DecisionTree:
+    def __init__(self, criterion="entropy", max_depth=None):
+        self.criterion = criterion
+        self.max_depth = max_depth
+        self.root = None  # holds the tree structure once fit() executes
+
+    def fit(self, X, y):
+        ...
+
+    def predict(self, X):
+        ...
+
+
+def entropy(y):
+    ...
+
+
+def best_split(X, y, impurity_function):
+    ...
+
+
+def build_tree(X, y, depth, max_depth, impurity_function):
+    # stoppbetingelser her, blant annet:
+    # 1.3 Maximum Depth: hvis depth == max_depth -> løvnode med mest vanlige label
+    ...
+
+
+def predict_one(x, node):
+    ...
+
+
+#1.2 The Gini Index
+
+def gini(y):
+    ...
