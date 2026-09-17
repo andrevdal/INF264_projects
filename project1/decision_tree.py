ced896fe60e6c86fa535a3b11d5663892a3ea58d
Amy <amykaletabe@gmail.com>
2026-09-18 01:47:43 +0200

    start bestSplit function

--- project1/decision_tree.py
+++ project1/decision_tree.py
@@ -45,8 +45,23 @@ def entropy(y):
         
     return accumulatedSum
 
-def bestSplit(X, y, impurityFunction):
-    ...
+def bestSplit(X, y, impurityFunction): #X = features, y = labels, impurityFunction = entropy/gini
+    #regn ut impurity for hele y, skal sammenlignes
+    #for hver feature (kolonne i x) finn terskel (gjennomsnitt eller median)
+    # del y i to grupper basert på om verdien til hver datapunkt er mindre enn/= eller større enn terskel
+    # regn ut vektet impurity til begge gruppene
+    # trekk fra første var
+    # if sjekk på om en av splittene er 0, hopp over denne kandidaten
+
+    impurityBeforeSplit = impurityFunction(y)
+
+    for i in range(X.shape[1]):
+        columnValues = X[:, i] # gets each column from the features
+        threshold = np.mean(columnValues) 
+
+        
+
+
 
 
 def buildTree(X, y, depth, maxDepth, impurityFunction):
