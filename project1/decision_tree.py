0bb9c21342e594feb10ffea69f12b31584b2c102
Amy <amykaletabe@gmail.com>
2026-09-17 23:59:14 +0200

    change variable names to camelBack

--- project1/decision_tree.py
+++ project1/decision_tree.py
@@ -1,13 +1,11 @@
 # 1 Decision Trees ---
-
 import numpy as np
 
 #1.1 The ID3 Algorithm 
-
 class DecisionTree:
-    def __init__(self, criterion="entropy", max_depth=None):
+    def __init__(self, criterion="entropy", maxDepth=None):
         self.criterion = criterion
-        self.max_depth = max_depth
+        self.maxDepth = maxDepth
         self.root = None  # holds the tree structure once fit() executes
 
     def fit(self, X, y):
@@ -18,24 +16,32 @@ class DecisionTree:
 
 
 def entropy(y):
+    # for each unique class c in y:
+    #     p = andel av y som er c
+    #     akkumuler -p * log2(p)   (hopp over p=0)
+    # return akkumulert sum
+    
+    uniqueClasses, numberOfEach = 
+
+
+def best_split(X, y, impurityFunction):
     ...
 
 
-def best_split(X, y, impurity_function):
+def buildTree(X, y, depth, maxDepth, impurityFunction):
+    #1.3 Maximum Depth: 
+    # hvis depth == max_depth -> løvnode med mest vanlige label
     ...
 
 
-def build_tree(X, y, depth, max_depth, impurity_function):
-    # stoppbetingelser her, blant annet:
-    # 1.3 Maximum Depth: hvis depth == max_depth -> løvnode med mest vanlige label
-    ...
-
-
-def predict_one(x, node):
+def predictOne(x, node):
     ...
 
 
 #1.2 The Gini Index
-
 def gini(y):
-    ...
+    # sum = 1
+    # for each unique class c in y:
+    #     p = andel av y som er c
+    #     sum = sum - p^2
+    # return sum
